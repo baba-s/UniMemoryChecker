f128ab3fd7cf010897f9d8cd312da967268f30f9
baba_s <shotababa1206@gmail.com>
2020-07-18 10:00:02 +0900

    fix static

--- Scripts/UnityMemoryChecker.cs
+++ Scripts/UnityMemoryChecker.cs
@@ -5,7 +5,7 @@ namespace Kogane
 	/// <summary>
 	/// Unity の使用メモリを確認するクラス
 	/// </summary>
-	public sealed class UnityMemoryChecker
+	public static class UnityMemoryChecker
 	{
 		//================================================================================
 		// プロパティ(static)
@@ -13,31 +13,31 @@ namespace Kogane
 		/// <summary>
 		/// Unity によって割り当てられたメモリを返します
 		/// </summary>
-		public float Used => ( Profiler.GetTotalAllocatedMemoryLong() >> 10 ) / 1024f;
+		public static float Used => ( Profiler.GetTotalAllocatedMemoryLong() >> 10 ) / 1024f;
 
 		/// <summary>
 		/// 予約済みだが割り当てられていないメモリを返します
 		/// </summary>
-		public float Unused => ( Profiler.GetTotalUnusedReservedMemoryLong() >> 10 ) / 1024f;
+		public static float Unused => ( Profiler.GetTotalUnusedReservedMemoryLong() >> 10 ) / 1024f;
 
 		/// <summary>
 		/// Unity が現在および将来の割り当てのために確保している総メモリを返します
 		/// </summary>
-		public float Total => ( Profiler.GetTotalReservedMemoryLong() >> 10 ) / 1024f;
+		public static float Total => ( Profiler.GetTotalReservedMemoryLong() >> 10 ) / 1024f;
 
 		/// <summary>
 		/// Unity によって割り当てられたメモリを返します
 		/// </summary>
-		public string UsedText => Used.ToString( "0.0" ) + " MB";
+		public static string UsedText => Used.ToString( "0.0" ) + " MB";
 
 		/// <summary>
 		/// 予約済みだが割り当てられていないメモリを返します
 		/// </summary>
-		public string UnusedText => Unused.ToString( "0.0" ) + " MB";
+		public static string UnusedText => Unused.ToString( "0.0" ) + " MB";
 
 		/// <summary>
 		/// Unity が現在および将来の割り当てのために確保している総メモリを返します
 		/// </summary>
-		public string TotalText => Total.ToString( "0.0" ) + " MB";
+		public static string TotalText => Total.ToString( "0.0" ) + " MB";
 	}
 }
