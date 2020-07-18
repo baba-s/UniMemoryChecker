94535f36f927f8530a90c5c37ae2a5944d6410fa
baba_s <shotababa1206@gmail.com>
2020-07-18 09:57:54 +0900

    fix static

--- Scripts/MonoMemoryChecker.cs
+++ Scripts/MonoMemoryChecker.cs
@@ -5,29 +5,17 @@ namespace Kogane
 	/// <summary>
 	/// Mono の使用メモリを確認するクラス
 	/// </summary>
-	public sealed class MonoMemoryChecker
+	public static class MonoMemoryChecker
 	{
 		//================================================================================
-		// プロパティ
+		// プロパティ(static)
 		//================================================================================
-		public float Used  { get; private set; }
-		public float Total { get; private set; }
+		public static float Used => ( Profiler.GetMonoUsedSizeLong() >> 10 ) / 1024f;
 
-		public string UsedText  { get; private set; }
-		public string TotalText { get; private set; }
+		// ヒープが大きいほど GC の時間がかかりますが、実行される頻度は少なくなる
+		public static float Total => ( Profiler.GetMonoHeapSizeLong() >> 10 ) / 1024f;
 
-		//================================================================================
-		// 関数
-		//================================================================================
-		public void Update()
-		{
-			Used = ( Profiler.GetMonoUsedSizeLong() >> 10 ) / 1024f;
-
-			// ヒープが大きいほど GC の時間がかかりますが、実行される頻度は少なくなる
-			Total = ( Profiler.GetMonoHeapSizeLong() >> 10 ) / 1024f;
-
-			UsedText  = Used.ToString( "0.0" ) + " MB";
-			TotalText = Total.ToString( "0.0" ) + " MB";
-		}
+		public static string UsedText  => Used.ToString( "0.0" ) + " MB";
+		public static string TotalText => Total.ToString( "0.0" ) + " MB";
 	}
 }
--- Scripts/UnityMemoryChecker.cs
+++ Scripts/UnityMemoryChecker.cs
@@ -8,58 +8,36 @@ namespace Kogane
 	public sealed class UnityMemoryChecker
 	{
 		//================================================================================
-		// プロパティ
+		// プロパティ(static)
 		//================================================================================
 		/// <summary>
 		/// Unity によって割り当てられたメモリを返します
 		/// </summary>
-		public float Used { get; private set; }
+		public float Used => ( Profiler.GetTotalAllocatedMemoryLong() >> 10 ) / 1024f;
 
 		/// <summary>
 		/// 予約済みだが割り当てられていないメモリを返します
 		/// </summary>
-		public float Unused { get; private set; }
+		public float Unused => ( Profiler.GetTotalUnusedReservedMemoryLong() >> 10 ) / 1024f;
 
 		/// <summary>
 		/// Unity が現在および将来の割り当てのために確保している総メモリを返します
 		/// </summary>
-		public float Total { get; private set; }
+		public float Total => ( Profiler.GetTotalReservedMemoryLong() >> 10 ) / 1024f;
 
 		/// <summary>
 		/// Unity によって割り当てられたメモリを返します
 		/// </summary>
-		public string UsedText { get; private set; }
+		public string UsedText => Used.ToString( "0.0" ) + " MB";
 
 		/// <summary>
 		/// 予約済みだが割り当てられていないメモリを返します
 		/// </summary>
-		public string UnusedText { get; private set; }
+		public string UnusedText => Unused.ToString( "0.0" ) + " MB";
 
 		/// <summary>
 		/// Unity が現在および将来の割り当てのために確保している総メモリを返します
 		/// </summary>
-		public string TotalText { get; private set; }
-
-		//================================================================================
-		// 関数
-		//================================================================================
-		/// <summary>
-		/// 現在のメモリの情報を取得します
-		/// </summary>
-		public void Update()
-		{
-			// Unity によって割り当てられたメモリ
-			Used = ( Profiler.GetTotalAllocatedMemoryLong() >> 10 ) / 1024f;
-
-			// 予約済みだが割り当てられていないメモリ
-			Unused = ( Profiler.GetTotalUnusedReservedMemoryLong() >> 10 ) / 1024f;
-
-			// Unity が現在および将来の割り当てのために確保している総メモリ
-			Total = ( Profiler.GetTotalReservedMemoryLong() >> 10 ) / 1024f;
-
-			UsedText   = Used.ToString( "0.0" ) + " MB";
-			UnusedText = Unused.ToString( "0.0" ) + " MB";
-			TotalText  = Total.ToString( "0.0" ) + " MB";
-		}
+		public string TotalText => Total.ToString( "0.0" ) + " MB";
 	}
 }
